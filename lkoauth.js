4db0ab24c641a4912ad583fa76308c80fd4818b2
Brian Egan <brian@brianegan.com>
2014-01-07 17:05:38 -0800

    Code cleanup, style consistency, prevents global leaks

--- lkoauth.js
+++ lkoauth.js
@@ -13,174 +13,185 @@
         });
     }
     else {
-        // Browser globals for those that don't support AMD
         win.LaunchKey = factory();
     }
 }(window, function() {
-
-    // in an anonymous function. This minimizes your footprint on the customer's site,
-    // and ensures your functions won't be overwritten later, which would break your code.
-    var oauthParams = {},
-        queryString = location.hash.substring(1),
-        regex = /([^&=]+)=([^&]*)/g,
-        oAuthUrl = "https://oauth.launchkey.com/",
-        m,
-        user = '',
-        state,
-        accessToken = '';
+    var OAUTH_ENDPOINT = 'https://oauth.launchkey.com/',
+        doc = document,
+        win = window;
 
     /**
-    * @private
-    * Sets a cookie
-    * @param {String} name The name of the cookie to set
-    * @param {String} cookieValue The value of the cookie being set
-    * @param {[type]} expireTime The time the cookie will expire
-    */
-    function setCookie(name, cookieValue, expireTime) {
-        var today = new Date();
-        var expire = new Date();
-        expire.setTime(today.getTime() + 3600000 * expireTime);
-        document.cookie = name + "=" + cookieValue + ";expires=" + expire.toUTCString() + ";";
-        var secure = window.location.protocol === 'https:';
-        if (secure === true) {
-            document.cookie += "secure;"
+     * @private
+     * Writes a cookie with a given name and value.
+     *
+     * @param {String} name The name of the cookie to set
+     * @param {String} value The value of the cookie being set
+     * @param {Number=} hours The time until the cookie will expire in hours
+     * @param {String=} domain The domain on which to set the cookie.
+     * @param {String=} path The path on which to set the cookie.
+     *
+     * @return {undefined}
+     */
+    function setCookie(name, value, hours, domain, path) {
+        var expires = '',
+            date = new Date();
+
+        hours = typeof hours === 'number' ? hours : 1;
+        domain = domain && domain !== '' ? '; domain=' + domain : '';
+        path = path && path !== '' ? '; path=' + path : '; path=/';
+
+        if (hours) {
+            date.setTime(Date.now() + (hours * 3600000));
+            expires = '; expires=' + date.toUTCString();
         }
+
+        doc.cookie = name + '=' + value + expires + path + domain;
     }
 
     /**
-    * @private
-    * Reads a given cookie
-    * @param  {String} name The name of the cookie to read
-    * @return {String} The value of the requested cookie
-    */
-    function readCookie(name) {
-        var cookie = " " + document.cookie;
-        var ind = cookie.indexOf(" " + name + "=");
-        if (ind === -1) ind = cookie.indexOf(";" + name + "=");
-        if (ind === -1 || name == "") return "";
-        var ind1 = cookie.indexOf(";", ind + 1);
-        if (ind1 === -1) ind1 = cookie.length;
-        return cookie.substring(ind + name.length + 2, ind1);
+     * @private
+     * Returns the value of a given cookie.
+     *
+     * @param  {String} name The name of the cookie to read
+     *
+     * @return {String|Undefined} The value of the requested cookie
+     */
+    function getCookie(name) {
+        var cookie = doc.cookie.split(name + '=')[1];
+
+        if (cookie) {
+            cookie = cookie.split(';')[0];
+        }
+
+        return cookie;
     }
 
     /**
     * @private
     * Deletes a given cookie
     * @param  {String} name The cookie to delete
+    *
     * @return {undefined}
     */
     function deleteCookie(name) {
-        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        setCookie(name, '', -1);
     }
 
     /**
      * @private
      * Framework independent function to load CSS
      * @param path To the CSS file
+     *
      * @returns {HTMLElement}
      */
     function loadStyleSheet(path) {
-       var head = document.getElementsByTagName( 'head' )[0], // reference to document.head for appending/ removing link nodes
-           link = document.createElement( 'link' );           // create the link node
-       link.setAttribute( 'id', 'cssLKLoad'); // set an attribute to
-       link.setAttribute( 'href', path );
-       link.setAttribute( 'rel', 'stylesheet' );
-       link.setAttribute( 'type', 'text/css' );
+       var head = doc.getElementsByTagName('head')[0],
+           link = doc.createElement('link');
 
-       var sheet, cssRules;
-       // get the correct properties to check for depending on the browser
-       if ( 'sheet' in link ) {
-          sheet = 'sheet'; cssRules = 'cssRules';
-       }
-       else {
-          sheet = 'styleSheet'; cssRules = 'rules';
-       }
+       link.setAttribute('id', 'cssLKLoad');
+       link.setAttribute('href', path);
+       link.setAttribute('rel', 'stylesheet');
+       link.setAttribute('type', 'text/css');
 
-       head.appendChild( link );  // insert the link node into the DOM and start loading the style sheet
+       return head.appendChild(link);
+    }
 
-       return link; // return the link node;
+    /**
+     * @private
+     * Gets the oAuth parameters
+     */
+    function getParams() {
+        var params = {},
+            queryString = location.hash.substring(1),
+            regex = /([^&=]+)=([^&]*)/g,
+            match;
+
+        while (match = regex.exec(queryString)) {
+            params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+        }
+
+        return params;
     }
 
     /**
     * @public
     * Logs the user out of their current session
+    * @param {Object} options The options available to the customer
+    * @param {Boolean=} options.reload Whether or not to reload the page after logout
+    * @param {Function=} options.callback A callback function to be fired after logout is successful
+    *
     * @return {undefined}
     */
-    function logout() {
-        accessToken = readCookie("LKlogin");
-        window.open(oAuthUrl + "logout?close=true&access_token=" + accessToken,
-                'LaunchKey OAuth logout','height=200,width=150').blur();
-        window.focus();
-        deleteCookie("LKlogin");
-        deleteCookie("LKuser");
-        document.location.reload(true);
+    function logout(options) {
+        var accessToken = getCookie('LKlogin'),
+            reload = options.hasOwnProperty('reload') ? options.reload : true,
+            callback = options.callback && typeof options.callback === 'function' ? options.callback : undefined;
+
+        win.open(OAUTH_ENDPOINT + 'logout?close=true&access_token=' + accessToken, 'LaunchKey OAuth logout','height=200,width=150').blur();
+        win.focus();
+
+        deleteCookie('LKlogin');
+        deleteCookie('LKuser');
+
+        if (reload) {
+            doc.location.reload(true);
+        }
+
+        if (callback) {
+            callback();
+        }
     }
 
     /**
-    * @public
-    *
-    * Add the LaunchKey button to your page.
-    *
-    * @param {Object} options The options available to the customer
-    * @param {Number} options.appKey The unique key for your application. No default.
-    * @param {String} options.size The size of the icon. Default is "large"
-    * @param {String} options.length Text length. Default is "full" for "Log in with LaunchKey". "Log in" is "short" and no text for "mini"
-    * @param {String} options.color The color of the button. Default is "blue". "light" and "dark" supported
-    * @param {String} options.redirectUrl Where the customer should go once logged in. Default is the current page
-    * @param {String} options.el The element you want to turn into a LaunchKey button. Default is an element with the id="lkLoginButton"
-    * @return {undefined}
-    */
+     * @public
+     * Add the LaunchKey button to your page.
+     *
+     * @param {Object} options The options available to the customer
+     * @param {Number=} options.appKey The unique key for your application. No default.
+     * @param {String=} options.size The size of the icon. Default is "large"
+     * @param {String=} options.length Text length. Default is "full" for "Log in with LaunchKey". "Log in" is "short" and no text for "mini"
+     * @param {String=} options.color The color of the button. Default is "blue". "light" and "dark" supported
+     * @param {String=} options.el The element you want to turn into a LaunchKey button. Default is an element with the id="lkLoginButton"
+     * @param {String=} options.redirectUrl Where the customer should go once logged in. Default is the current page
+     *
+     * @return {undefined}
+     */
     function init(options) {
-    // Sets the defaults
-    var color = options.color ? options.color.toLowerCase() : 'blue',
-        size = options.size ? options.size.toLowerCase() : 'large',
-        length = options.length ? options.length.toLowerCase() : 'full',
-        el = options.el || document.getElementById('lkLoginButton');
-    var defaultRedirectUrl = encodeURIComponent(location.protocol + '//' + location.host + window.location.pathname);
-    var redirectUrl = options.redirectUrl ? encodeURIComponent(options.redirectUrl) : defaultRedirectUrl;
+        var state = getCookie('LKstate'),
+            params = getParams(),
 
-    var authorized = false;
+            // Options
+            size = options.size ? options.size.toLowerCase() : 'large',
+            length = options.length ? options.length.toLowerCase() : 'full',
+            color = options.color ? options.color.toLowerCase() : 'blue',
+            el = options.el || doc.getElementById('lkLoginButton'),
 
-    while (m = regex.exec(queryString)) {
-      oauthParams[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
-    }
+            defaultRedirectUrl = encodeURIComponent(location.protocol + '//' + location.host + win.location.pathname),
+            redirectUrl = options.redirectUrl ? encodeURIComponent(options.redirectUrl) : defaultRedirectUrl;
 
-    state = readCookie("LKstate");
-
-    if (oauthParams.state === state) {
-        setCookie("LKlogin", oauthParams.access_token, oauthParams.expires_in);
-        setCookie("LKuser", oauthParams.user_id, oauthParams.expires_in);
-        //Session exists, user is logged in
-        authorized = true;
-    }
-    else {
-        accessToken = readCookie("LKlogin");
-        user = readCookie("LKuser");
-        if (accessToken !== "" && user !== "") {
-            //Session exists, user is logged in
-            authorized = true;
-        }
+    if (params.state === state) {
+        // The setCookie function is expecting the expire time in hours, but if this is returning seconds stuff could get weird
+        setCookie('LKlogin', params.access_token, params.expires_in);
+        setCookie('LKuser', params.user_id, params.expires_in);
     }
 
     state = Math.max(0.01, Math.random()).toString(36).slice(2);
-    setCookie("LKstate", state, 1);
 
-    if (readCookie("LKstate") !== state) {
-        state = "";
-    }
+    // Should this time to expire be configurable?
+    setCookie('LKstate', state, 1);
 
+    // Appending the button could be optional
     if (el) {
-        el.innerHTML = "<a id=\"lkButton\" href=\"" + oAuthUrl + "authorize?client_id=" + encodeURIComponent(options.appKey.toString()) + "&redirect_uri=" + redirectUrl + "&response_type=token&scope=&state=" + encodeURIComponent(state) + "\" class=\"lkloginbtn " + length + " " + color + " " + size + "\"><span class=\"icon\"></span></a>";
-        var $ = document;
+        el.innerHTML = '<a id="lkButton" href="' + OAUTH_ENDPOINT + 'authorize?client_id=' + encodeURIComponent(options.appKey.toString()) + '&redirect_uri=' + redirectUrl + '&response_type=token&scope=&state=' + encodeURIComponent(state) + '" class="lkloginbtn ' + length + ' ' + color + ' ' + size + '"><span class="icon"></span></a>';
         var cssLoad = 'cssLKLoad';
-        if (!$.getElementById(cssLoad)) {
-            loadStyleSheet("https://launchkey.com/stylesheets/buttons.css");
+        if (!doc.getElementById(cssLoad)) {
+            loadStyleSheet('https://launchkey.com/stylesheets/buttons.css');
         }
-        if (length === "short") {
-            document.getElementById('lkButton').innerHTML += "<span class=\"text\">Log in</span>";
+        if (length === 'short') {
+            doc.getElementById('lkButton').innerHTML += '<span class="text">Log in</span>';
         }
-        else if (length === "full") {
-            document.getElementById('lkButton').innerHTML += "<span class=\"text\">Log in with LaunchKey</span>";
+        else if (length === 'full') {
+            doc.getElementById('lkButton').innerHTML += '<span class="text">Log in with LaunchKey</span>';
         }
     }
   }
